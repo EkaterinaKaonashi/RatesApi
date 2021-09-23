cdb6963eef1d83f312a0018ac667227e8484af55
Vadim <vki-13@yandex.ru>
2021-09-23 13:37:26 +0300

    TestsProj

---
+++ RatesApiTests/RatesGatterTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RatesApiTests
+{
+    public class RatesGatterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

4283aabf88db34918325d6dd828243b0b3d9883b
Vadim <vki-13@yandex.ru>
2021-09-23 17:03:50 +0300

    AddedRatesGetterTests

--- RatesApiTests/TestData/SettingsData.cs
+++ RatesApiTests/TestData/SettingsData.cs
@@ -16,5 +16,35 @@ namespace RatesApiTests.TestData
                 Currencies = new List<string> { "RUB", "EUR", "JPY" }
             };
         }
+
+        public static CommonSettings GetCommonSettingsWithWrongCurrency()
+        {
+            return new CommonSettings
+            {
+                MillisecondsDelay = 3600000,
+                RetryTimeout = 3600,
+                RetryCount = 3,
+                BaseCurrency = "USD",
+                Currencies = new List<string> { "RUB", "EUR", "JPY", "XXX" }
+            };
+        }
+
+        public static PrimaryRatesGetterSettings GetPrimaryRatesGetterSettings()
+        {
+            return new PrimaryRatesGetterSettings
+            {
+                AccessKey = "PRIMARY_ACCESS_KEY",
+                Url = "https://fcurrencyapi.net/api/v1/rates?key={0}&output=JSON&base={1}"
+            };
+        }
+
+        public static SecondaryRatesGetterSettings GetSecondaryRatesGetterSettings()
+        {
+            return new SecondaryRatesGetterSettings
+            {
+                AccessKey = "SECONDARY_ACCESS_KEY",
+                Url = "https://fopenexchangerates.org/api/latest.json?app_id={0}&base={1}"
+            };
+        }
     }
 }
